1771384420a0f634177468b70e4adc6720440fca
Tarun Mathur <tarun.mathur01@infosys.com>
2024-09-15 12:18:43 +0530

    x-pagination

---
+++ src/components/EmployeeTable.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import styles from "./EmployeeTable.module.css";
+import axios from "axios";
+
+const EmployeeTable = () => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        setData(response.data);
+      } catch (error) {
+        setError("Failed to fetch data");
+        alert("failed to fetch data");
+      }
+    };
+    fetchData();
+  }, []);
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentPageData = data.slice(startIndex, startIndex + itemsPerPage);
+
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.tableBody}>
+        {error && <p>{error}</p>}
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody className={styles.tbody}>
+            {currentPageData.map((employee, index) => (
+              <tr>
+                <td>{employee.id}</td>
+                <td>{employee.name}</td>
+                <td>{employee.email}</td>
+                <td>{employee.role}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button onClick={handlePrevious} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <button>{currentPage}</button>
+        <button onClick={handleNext} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default EmployeeTable;
